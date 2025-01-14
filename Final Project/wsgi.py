42f3c28ce07e661f2e916281cdd2bd83b2c6f7c3
Szymon Bryniak <szymonbryniakproject@gmail.com>
2025-01-14 19:03:47 +0000

    Day 56 progress

---
+++ Final Project/wsgi.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def website():
+  return render_template("index.html")
+
+
+if __name__ == "__name__":
+  app.run(debug=True)
